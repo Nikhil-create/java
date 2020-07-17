c9c94a855c14aad231df53e61ae1a28198566094
Nikhil Rawal <60665871+Nikhil-create@users.noreply.github.com>
2020-07-17 14:03:27 +0530

    Add files via upload

---
+++ DuplicateInArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class DuplicateInArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i=0;
+		int num=sc.nextInt();
+		int[] arr=new int[num];
+		for(i=0;i<num;i++) {
+			arr[i]=sc.nextInt();
+		}
+		for(i=0;i<num;i++) {
+			if(arr[Math.abs(arr[i])]>=0)
+				arr[Math.abs(arr[i])]=-arr[Math.abs(arr[i])];
+			else
+				System.out.print(-arr[Math.abs(arr[i])]+" ");
+		}
+
+
+	}
+
+}
